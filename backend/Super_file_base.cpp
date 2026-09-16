7330825845af7285d86863cd47e32bd1ed05eff1
programmer9129 <programmer9129@gmail.com>
2026-09-16 14:04:23 +0530

    1st work

---
+++ backend/Super_file_base.cpp
@@ -0,0 +1,12 @@
+﻿// Super_file_base.cpp : Defines the entry point for the application.
+//
+
+#include "Super_file_base.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
