3f791995e644c5e998010599972461cde48640bb
programmer9129 <programmer9129@gmail.com>
2026-09-17 09:02:47 +0530

    updated backend 1st time

--- backend/Super_file_base.cpp
+++ backend/Super_file_base.cpp
@@ -2,11 +2,59 @@
 //
 
 #include "Super_file_base.h"
+#include "crow.h"
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/")([]
+	{
+		return "Hello, World!";
+		return "hello, this is cmake crow c++ hoster web please , respond trigger 000900090009000 dont care random number tho... ";
+	});
+
+	CROW_ROUTE(app, "/json")
+		.methods(crow::HTTPMethod::POST)
+
+		([](const crow::request& req)
+			{
+				auto body = crow::json::load(req.body);
+
+				if (!body)
+				{
+					crow::json::wvalue error;
+					error["success"] = false;
+					error["error"] = "Invalid JSON";
+
+					return crow::response(400, error);
+				}
+
+				if (!body["input"])
+				{
+					crow::json::wvalue error;
+					error["success"] = false;
+					error["error"] = "Missing input";
+
+					return crow::response(400, error);
+				}
+
+				std::string userinput = body["input"].s();
+				crow::json::wvalue response;
+
+				response["success"] = true;
+				response["output"] = "C++ crow server is on hot action buddy" + userinput;
+				return crow::response(response);
+
+
+			});
+
+			
+
+	app.port(8080).multithreaded().run();
+
 }
+
