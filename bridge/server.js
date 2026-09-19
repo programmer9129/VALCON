40ae7215f40c9f70fd5f459caae5daf952a566b5
programmer9129 <programmer9129@gmail.com>
2026-09-19 10:38:29 +0530

    database established

--- bridge/server.js
+++ bridge/server.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const {createClient} = require("@supabase/supabase-js");
+
 const ID_BACKEND_ADRESS = "http://localhost:8080/server_bridge";
 //const ID_BACKEND_ADRESS_LIVE =
 const app = express();
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SECRET_KEY);
+
+
 const PORT = 3000;
+const BACKEND_URL_TRUE = "http://localhost:8080";
+
 app.use(express.json());
 
 app.get("/server_bridge", (req, res) => {
@@ -29,6 +40,29 @@ app.get("/server_bridge/cpp", async (req ,res) =>
        console.log(err);
        console.log('C++ brain is unable to connect to the bridge');
    }
+});
+app.get("/server_bridge/supabase", async (req, res) =>{
+    try {
+        const {data , error} = await supabase
+            .storage
+            .listBuckets();
+
+        if (error)
+        {
+            throw error;
+        }
+
+        res.json({
+            success: true,
+            service: "supabase",
+            status: "ok",
+            bucket: data
+        })
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
 });
 
 app.listen(PORT, () =>{
