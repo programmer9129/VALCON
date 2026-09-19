e06e2152845e0de725f200f5f54a5915f6f08b67
programmer9129 <programmer9129@gmail.com>
2026-09-19 09:54:42 +0530

    calculator updated and file_system work has been started

--- bridge/server.js
+++ bridge/server.js
@@ -1,11 +1,36 @@
-const ID_BACKEND_ADRESS = "http//localhost:8080/health";
+const express = require("express");
+const ID_BACKEND_ADRESS = "http://localhost:8080/server_bridge";
 //const ID_BACKEND_ADRESS_LIVE =
+const app = express();
+const PORT = 3000;
+app.use(express.json());
 
-
-app.get("/health", (req, res) => {
+app.get("/server_bridge", (req, res) => {
     res.json({
         status: "ok",
         service: "node",
         message: "BRIGE SERVICE IS AVAILABLE"
     });
+})
+
+app.get("/server_bridge/cpp", async (req ,res) =>
+{
+   try{
+       const response = await fetch(ID_BACKEND_ADRESS);
+
+       const data = await response.json();
+       res.json({
+          success: true,
+          node: "node is working",
+          cpp: data
+       });
+   }
+   catch(err){
+       console.log(err);
+       console.log('C++ brain is unable to connect to the bridge');
+   }
+});
+
+app.listen(PORT, () =>{
+   console.log('bridge is running on port' + PORT);
 });
