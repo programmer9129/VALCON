6a26da3c64b13ebb739da6a0eec7180786941076
programmer9129 <programmer9129@gmail.com>
2026-09-20 15:03:51 +0530

    Update Super_file_base.cpp

--- backend/Super_file_base.cpp
+++ backend/Super_file_base.cpp
@@ -246,7 +246,7 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmeb, void* user
 		return 0;
 	size_t total = size * nmeb;
 
-	std::stirng* response = static_cast<std::string*>(userp);
+	std::string* response = static_cast<std::string*>(userp);
 
 	response->append(
 		static_cast<const char*>(contents),
