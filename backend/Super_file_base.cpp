e8fcb2c0e17a30ed0fa281cf30127ca775e6ee29
programmer9129 <programmer9129@gmail.com>
2026-09-20 16:39:22 +0530

    Update Super_file_base.cpp

--- backend/Super_file_base.cpp
+++ backend/Super_file_base.cpp
@@ -229,7 +229,7 @@ string processstrings(string order_commands)
 		std::string result_of_read = BRIDGERequest("read", ichyname);
 		try
 		{
-			crow::json READING_FILE = json::parse(result_of_read);
+			json READING_FILE = json::parse(result_of_read);
 			if (READING_FILE["success"] == true)
 			{
 				return READING_FILE["content"].get<string>();
